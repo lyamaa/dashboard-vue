c8cc09359b6ce04e0a08ec3320995cefd2e5e59f
surajmt8848 <suraj8848mt@gmail.com>
2021-01-07 00:46:18 +0545

    re structure modules

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+  <Layout />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+
+import Layout from "@/views/Layout/Layout.vue"
 
 export default defineComponent({
   name: 'Home',
   components: {
-    HelloWorld,
+   Layout,
   },
 });
 </script>
+
+<style src="../_main.scss" lang="scss">
+
+</style>
---
+++ src/views/Layout/Layout.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <Navbar />
+
+    <div class="container">
+      <div class="columns">
+        <Menu />
+        <Dashboard />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Menu from "@/components/Menu.vue";
+import Navbar from "@/components/Navbar.vue";
+import Dashboard from "@/components/Dashboard.vue";
+
+export default defineComponent({
+  name: "Layout",
+  components: {
+    Navbar,
+    Menu,
+    Dashboard,
+  },
+});
+</script>
