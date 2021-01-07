83913a68d020c52c961f0c91153c2fea200be295
surajmt8848 <suraj8848mt@gmail.com>
2021-01-07 21:49:47 +0545

    create section components

---
+++ src/components/Section.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="hero is-info welcome is-small">
+    <div class="hero-body">
+      <div class="container">
+        <h1 class="title">
+          Hello, <strong></strong>
+        </h1>
+        <h2 class="subtitle">Welcome to dashboard!</h2>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Section",
+  
+};
+</script>
