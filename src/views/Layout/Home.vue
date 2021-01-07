7eb0681d08b7e99f9be9b7ad9559e0c4f06ac778
surajmt8848 <suraj8848mt@gmail.com>
2021-01-07 18:32:03 +0545

    re structured page

---
+++ src/views/Layout/Home.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <Navbar :user="user" />
+    <div class="container">
+      <div class="columns">
+        <Menu />
+       
+        <router-view />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { onMounted, ref } from "vue";
+import { defineComponent } from "vue";
+import Menu from "../../components/Menu.vue";
+import Navbar from "../../components/Navbar.vue";
+import Dashboard from "../../components/Dashboard.vue";
+
+import axios from "axios";
+import { useRouter } from "vue-router";
+
+export default defineComponent({
+  name: "Layout",
+  components: {
+    Navbar,
+    Menu,
+    Dashboard,
+  },
+  setup() {
+    const router = useRouter()
+    const user = ref('')
+    onMounted(async () => {
+      try {
+        const res = await axios.get("user");
+
+        user.value = res.data.data
+        console.log(res);
+      } catch (err) {
+        await router.push('/login')
+      }
+    });
+
+    return {
+      user
+    }
+  },
+});
+</script>
+
+<style src="../../_main.scss" lang="scss">
+
+</style>
